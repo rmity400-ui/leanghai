31a3ca46998dbb8012d9c6c12d08a46d22d621da
school-website-app-now <rmity400@gmail.com>
2026-07-14 11:38:22 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // បន្ថែមបន្ទាត់នេះ
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // បន្ថែមពាក្យនេះក្នុង plugins
+  ],
 })
